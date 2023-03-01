2ee0e8556aa72b2691a9ad85d4dfb9fd9cfc9eb8
tiagopcanas <tiagopcanas@hotmail.com>
2023-03-01 19:35:35 +0000

    get all nodes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/fc54464cn/realpython/flask-connexion-rest-part-3/venv"
+set -gx VIRTUAL_ENV "/home/fc54464cn/restapi/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
